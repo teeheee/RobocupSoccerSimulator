dd5b9f90075c9eb0a0cc0b63ed9d70f691525196
windows-vm <alexander.ulbrich@uni-ulm.de>
2017-07-20 23:24:57 +0200

    added win32 setup exe

--- setup.py
+++ setup.py
@@ -2,10 +2,22 @@
 
 import sys
 from cx_Freeze import setup, Executable
+import os.path
+
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+
 
 
 setup(
     name="Robocup Simulator",
-    options={"build_exe": {"packages":["pygame","numpy","pymunk"]}},
+    options={"build_exe": {"packages":["pygame","numpy","pymunk"],
+	   "include_files":[
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+	   "chipmunk.dll","Team1","Team2"]}},
     executables = [Executable("robotsimul.py")]
     )

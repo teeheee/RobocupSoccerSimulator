d009da66a67aa319e1153b33570bd4c91f0de210
Alexander Ulbrich <alexander.ulbrich@uni-ulm.de>
2017-07-21 00:02:02 +0200

    release 1.1

--- setup.py
+++ setup.py
@@ -3,21 +3,24 @@
 import sys
 from cx_Freeze import setup, Executable
 import os.path
+import platform
 
-
-PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
-
-
+if platform.system() == 'Windows':
+    PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+    os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+    os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+    setupOptions = {"build_exe": {"packages": ["pygame", "numpy", "pymunk"],
+                             "include_files": [
+                                 os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+                                 os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+                                 "chipmunk.dll", "Team1", "Team2"]}}
+else:
+    setupOptions = {"build_exe": {"packages": ["pygame", "numpy", "pymunk"],
+                    "include_files": ["libchipmunk.so", "Team1", "Team2"]}}
 
 
 setup(
     name="Robocup Simulator",
-    options={"build_exe": {"packages":["pygame","numpy","pymunk"],
-	   "include_files":[
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
-	   "chipmunk.dll","Team1","Team2"]}},
+    options=setupOptions,
     executables = [Executable("robotsimul.py")]
     )

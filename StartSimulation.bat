0f7dcb4d6fc6a61f07e31cff01ba160fc1524a1a
windows-vm <alexander.ulbrich@uni-ulm.de>
2017-12-20 20:47:42 +0100

    alternative config file

--- StartSimulation.bat
+++ StartSimulation.bat
@@ -1,3 +1,16 @@
-cd %~dp0
+@echo off
+if exist %cd%\config.yml (
+SET config = %cd%\config.yml
+cd /d %~dp0
+@echo on
+robotsimul.exe %config%
+pause
+) else (
+cd /d %~dp0
+echo Warnung!! config.yml wurde nicht gefunden. Nutze installations Standard.
+@echo on
 robotsimul.exe
 pause
+)
+
+
